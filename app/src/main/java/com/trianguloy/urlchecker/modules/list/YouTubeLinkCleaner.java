6352d1223cc9425204732b12435c300a38235902
Final Phoenix <126144597+Final-Phoenix@users.noreply.github.com>
2025-03-29 07:23:44 -0500

    Update YouTubeLinkCleaner.java

--- app/src/main/java/com/trianguloy/urlchecker/modules/list/YouTubeLinkCleaner.java
+++ app/src/main/java/com/trianguloy/urlchecker/modules/list/YouTubeLinkCleaner.java
@@ -1,36 +1,28 @@
 package com.trianguloy.urlchecker.modules.list;
 
+import android.content.Context;
 import android.net.Uri;
 import android.view.View;
-import android.widget.TextView;
 
 import com.trianguloy.urlchecker.R;
 import com.trianguloy.urlchecker.activities.GuiActivity;
+import com.trianguloy.urlchecker.activities.ModulesActivity;
 import com.trianguloy.urlchecker.dialogs.MainDialog;
+import com.trianguloy.urlchecker.modules.AModuleConfig;
+import com.trianguloy.urlchecker.modules.AModuleData;
 import com.trianguloy.urlchecker.modules.AModuleDialog;
+import com.trianguloy.urlchecker.modules.AutomationRules;
 import com.trianguloy.urlchecker.url.UrlData;
-import com.trianguloy.urlchecker.views.Getter;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Module that cleans YouTube links by removing tracking parameters
  */
-public class YouTubeLinkCleaner extends AModuleDialog {
-
-    // YouTube domains
-    private static final Set<String> YOUTUBE_DOMAINS = new HashSet<>(Arrays.asList(
-            "youtube.com", "www.youtube.com", "youtu.be"
-    ));
-
-    // Tracking parameters to remove
-    private static final Set<String> TRACKING_PARAMS = new HashSet<>(Arrays.asList(
-            "si", "feature", "utm_source", "utm_medium", "utm_campaign",
-            "utm_term", "utm_content", "fbclid", "ref", "mc_cid",
-            "mc_eid", "_ga", "gclid", "msclkid", "dclid"
-    ));
+public class YouTubeLinkCleaner extends AModuleData {
 
     @Override
     public String getId() {
@@ -43,22 +35,66 @@ public class YouTubeLinkCleaner extends AModuleDialog {
     }
 
     @Override
-    public boolean isEnabledByDefault() {
-        return true;
+    public AModuleDialog getDialog(MainDialog cntx) {
+        return new YouTubeLinkCleanerDialog(cntx);
     }
 
     @Override
-    public void onPrepareDialog(GuiActivity activity, MainDialog dialog) {
-        // No preparation needed
+    public AModuleConfig getConfig(ModulesActivity cntx) {
+        return new YouTubeLinkCleanerConfig(cntx);
     }
 
     @Override
-    public void onShowDialog(GuiActivity activity, MainDialog dialog) {
-        // No UI needed
+    public List<AutomationRules.Automation<AModuleDialog>> getAutomations() {
+        return null; // No automations needed
+    }
+}
+
+class YouTubeLinkCleanerConfig extends AModuleConfig {
+    public YouTubeLinkCleanerConfig(ModulesActivity activity) {
+        super(activity);
     }
 
     @Override
-    public void onUrlSelected(UrlData urlData, MainDialog dialog) {
+    public int getLayoutId() {
+        return 0; // No layout needed
+    }
+
+    @Override
+    public void onInitialize(View views) {
+        // No initialization needed
+    }
+}
+
+class YouTubeLinkCleanerDialog extends AModuleDialog {
+    // YouTube domains
+    private static final Set<String> YOUTUBE_DOMAINS = new HashSet<>(Arrays.asList(
+            "youtube.com", "www.youtube.com", "youtu.be"
+    ));
+
+    // Tracking parameters to remove
+    private static final Set<String> TRACKING_PARAMS = new HashSet<>(Arrays.asList(
+            "si", "feature", "utm_source", "utm_medium", "utm_campaign",
+            "utm_term", "utm_content", "fbclid", "ref", "mc_cid",
+            "mc_eid", "_ga", "gclid", "msclkid", "dclid"
+    ));
+
+    public YouTubeLinkCleanerDialog(MainDialog dialog) {
+        super(dialog);
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0; // No layout needed
+    }
+
+    @Override
+    public void onInitialize(View views) {
+        // No initialization needed
+    }
+
+    @Override
+    public void onModifyUrl(UrlData urlData, JavaUtils.Function<UrlData, Boolean> setNewUrl) {
         String url = urlData.url;
         Uri uri = Uri.parse(url);
         
@@ -79,6 +115,7 @@ public class YouTubeLinkCleaner extends AModuleDialog {
             
             // Update the URL with cleaned version
             urlData.url = builder.build().toString();
+            setNewUrl.apply(urlData);
         }
     }
 } 
